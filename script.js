7c9748475159cbf019c2bc73d256632bf63c76e6
devdani672 <danyperezrodri8@gmail.com>
2024-05-30 14:31:27 +0000

    funcion cartas a medias

--- script.js
+++ script.js
@@ -1,19 +1,28 @@
+import { baraja } from './scriptCartas.js'
+
 const menuInicial = document.getElementById("menu-inicial")
 const menuJugar = document.getElementById("menu-jugar")
+const carta1Imagen = document.getElementById("carta-1")
+const carta2Imagen = document.getElementById("carta-2")
+const carta1CrupierImagen = document.getElementById("carta-crupier-1")
+const carta2CrupierImagen = document.getElementById("carta-crupier-2")
+
+const botonApostar = document.getElementById("apostar-boton")
+const botonPedir = document.getElementById("pedir-boton")
+const botonPlantarse = document.getElementById("plantarse-boton")
+
+botonApostar.addEventListener("click",comprobarApuesta)
+botonPedir.addEventListener("click",pedirCarta)
+botonPlantarse.addEventListener("click",plantarse)
 
 let dinero = 100
 let dineroContador = document.getElementById("dinero-usuario");
 dineroContador.textContent = "Fondos: " + dinero + "€";
 
-let tuMano 
-let carta1 
-let carta2 
+let tuMano, carta1, carta2, manoCrupier, cartaCrupier1, cartaCrupier2, apuestaInicial
 let cartaNueva = 0
-let manoCrupier
-let cartaCrupier1
-let cartaCrupier2
-let cartaCrupierNueva
-let apuestaInicial
+let cartaCrupierNueva = 0
+
 
 function generarNumeroCarta() {
     const random = Math.random();
@@ -65,60 +74,52 @@ function comprobarApuesta() {
     }
 }
 
+function comprobarCarta(carta,valor) {
+    if (carta === carta1) {
+        carta1 = valor;
+    } else if (carta === carta2) {
+        carta2 = valor;
+    } else if (carta === cartaNueva) {
+        cartaNueva = valor;
+    } else if (carta === cartaCrupier1) {
+        cartaCrupier1 = valor;
+    } else if (carta === cartaCrupier2) {
+        cartaCrupier2 = valor;
+    } else if (carta === cartaCrupierNueva) {
+        cartaCrupierNueva = valor;
+    }
+}
+
 function comprobarAs(primera,segunda,nueva) {
     if (primera==1 && segunda==1){
         if (primera == carta1){
-            return carta1 = 1
-        } else {
-            return cartaCrupier1 = 1
+            return carta1 = 11
+        }else {
+            return cartaCrupier1 = 11
         }
     }
     if (primera==1){
         if (11+segunda>21){
-            if (primera == carta1){
-                carta1 = 1
-            } else {
-                cartaCrupier1 = 1
-            }
-        } else{
-            if (primera == carta1){
-                carta1 = 11
-            } else {
-                cartaCrupier1 = 11
-            }
+            comprobarCarta(primera,1)
+        }else {
+             comprobarCarta(primera,11)
         }   
     }
     if (segunda==1){
         if (11+primera>21){
-            if (segunda == carta2){
-                carta2 = 1
-            } else {
-                cartaCrupier2 = 1
-            }
-        } else{
-            if (segunda == carta2){
-                carta2 = 11
-            } else {
-                cartaCrupier2 = 11
-            }
-        }   
-    }
+            comprobarCarta(segunda,1)
+        }else {
+            comprobarCarta(segunda,11)
+            }   
+        }
     if (nueva==1){
         if (11+primera+segunda>21){
-            if (nueva == cartaNueva){
-                cartaNueva = 1
-            } else {
-                cartaCrupierNueva = 1
-            }
-        } else{
-            if (nueva == cartaNueva){
-                cartaNueva = 11
-            } else {
-                cartaCrupierNueva = 11
-            }
+             comprobarCarta(nueva,1)
+        }else {
+            comprobarCarta(nueva,11)
         }   
     }
-}
+    }
 
 function apostar() {
     carta1 = generarNumeroCarta()
@@ -126,7 +127,7 @@ function apostar() {
 
     comprobarAs(carta1,carta2,cartaNueva)
 
-    tuMano =  carta1 + carta2
+   
 
     cartaCrupier1 =  generarNumeroCarta()
     cartaCrupier2 =  generarNumeroCarta()
@@ -135,9 +136,41 @@ function apostar() {
 
     manoCrupier = cartaCrupier1 + cartaCrupier2
 
-    console.log(carta1, carta2, cartaCrupier1,cartaCrupier2)
+    console.log(carta1, carta2, cartaCrupier1, cartaCrupier2)
     console.log("Tu mano: " + tuMano)
     console.log("Mano crupier: " + manoCrupier)
+
+    let z = Math.floor(Math.random() * 16)
+    let y = Math.floor(Math.random() * 16)
+    let a = Math.floor(Math.random() *4)
+    let b = Math.floor(Math.random() *4)
+    let c = Math.floor(Math.random() *4)
+    let d = Math.floor(Math.random() *4)
+
+    console.log("A vale " +a)
+    console.log("B vale " +b)
+    console.log("C vale " +c)
+    console.log("D vale " +d)
+    console.log("Z vale " +z)
+    console.log("Y vale " +y)
+
+    if (carta1==10){
+        carta1Imagen.setAttribute("src","baraja-img/" + baraja[carta1][z])
+    } else if (carta2==10){
+        carta2Imagen.setAttribute("src","baraja-img/" + baraja[carta2][z])
+    } else{
+        carta1Imagen.setAttribute("src","baraja-img/" + baraja[carta1][a])
+        carta2Imagen.setAttribute("src","baraja-img/" + baraja[carta2][b])
+    }
+    
+    if (cartaCrupier1==10){
+        carta1CrupierImagen.setAttribute("src","baraja-img/" + baraja[cartaCrupier1][y])
+    } else if (cartaCrupier2==10){
+        carta2CrupierImagen.setAttribute("src","baraja-img/" + baraja[cartaCrupier2][y])
+    } else{
+        carta1CrupierImagen.setAttribute("src","baraja-img/" + baraja[cartaCrupier1][c])
+        carta2CrupierImagen.setAttribute("src","baraja-img/" + baraja[cartaCrupier2][d])
+    }
 
     menuInicial.style.display = "none";
     menuJugar.style.display = " inline-flex" 
