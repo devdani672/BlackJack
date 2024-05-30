78b1a71431b70c58d9cbed38e5b94b65dffaeb23
devdani672 <danyperezrodri8@gmail.com>
2024-05-30 14:48:55 +0000

    actualiizar cartas terminado al apostar

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
-import { baraja } from './scriptCartas.js'
+const baraja = {
+    2: ["2_of_clubs.png","2_of_diamonds.png","2_of_hearts.png","2_of_spades.png"],
+    3: ["3_of_clubs.png","3_of_diamonds.png","3_of_hearts.png","3_of_spades.png"],
+    4: ["4_of_clubs.png","4_of_diamonds.png","4_of_hearts.png","4_of_spades.png"],
+    5: ["5_of_clubs.png","5_of_diamonds.png","5_of_hearts.png","5_of_spades.png"],
+    6: ["6_of_clubs.png","6_of_diamonds.png","6_of_hearts.png","6_of_spades.png"],
+    7: ["7_of_clubs.png","7_of_diamonds.png","7_of_hearts.png","7_of_spades.png"],
+    8: ["8_of_clubs.png","8_of_diamonds.png","8_of_hearts.png","8_of_spades.png"],
+    9: ["9_of_clubs.png","9_of_diamonds.png","9_of_hearts.png","9_of_spades.png"],
+    10: ["10_of_clubs.png","10_of_diamonds.png","10_of_hearts.png","10_of_spades.png","jack_of_clubs2.png",
+        "jack_of_diamonds2.png","jack_of_hearts2.png","jack_of_spades2.png","king_of_clubs2.png",
+        "king_of_diamonds2.png","king_of_hearts2.png","king_of_spades2.png","queen_of_clubs2.png",
+        "queen_of_diamonds2.png","queen_of_hearts2.png","queen_of_spades2.png"],
+    11: ["ace_of_clubs.png","ace_of_diamonds.png","ace_of_hearts.png","ace_of_spades.png"],
+    1: ["ace_of_clubs.png","ace_of_diamonds.png","ace_of_hearts.png","ace_of_spades.png"]
+}
 
 const menuInicial = document.getElementById("menu-inicial")
 const menuJugar = document.getElementById("menu-jugar")
@@ -127,7 +142,7 @@ function apostar() {
 
     comprobarAs(carta1,carta2,cartaNueva)
 
-   
+    tuMano = carta1 + carta2
 
     cartaCrupier1 =  generarNumeroCarta()
     cartaCrupier2 =  generarNumeroCarta()
@@ -142,35 +157,35 @@ function apostar() {
 
     let z = Math.floor(Math.random() * 16)
     let y = Math.floor(Math.random() * 16)
+    let x = Math.floor(Math.random() * 16)
+    let w = Math.floor(Math.random() * 16)
     let a = Math.floor(Math.random() *4)
     let b = Math.floor(Math.random() *4)
     let c = Math.floor(Math.random() *4)
     let d = Math.floor(Math.random() *4)
 
-    console.log("A vale " +a)
-    console.log("B vale " +b)
-    console.log("C vale " +c)
-    console.log("D vale " +d)
-    console.log("Z vale " +z)
-    console.log("Y vale " +y)
-
     if (carta1==10){
         carta1Imagen.setAttribute("src","baraja-img/" + baraja[carta1][z])
-    } else if (carta2==10){
-        carta2Imagen.setAttribute("src","baraja-img/" + baraja[carta2][z])
     } else{
         carta1Imagen.setAttribute("src","baraja-img/" + baraja[carta1][a])
+    } 
+    if (carta2==10){
+        carta2Imagen.setAttribute("src","baraja-img/" + baraja[carta2][y])
+    } else {
         carta2Imagen.setAttribute("src","baraja-img/" + baraja[carta2][b])
     }
     
     if (cartaCrupier1==10){
-        carta1CrupierImagen.setAttribute("src","baraja-img/" + baraja[cartaCrupier1][y])
-    } else if (cartaCrupier2==10){
-        carta2CrupierImagen.setAttribute("src","baraja-img/" + baraja[cartaCrupier2][y])
+        carta1CrupierImagen.setAttribute("src","baraja-img/" + baraja[cartaCrupier1][x])
     } else{
         carta1CrupierImagen.setAttribute("src","baraja-img/" + baraja[cartaCrupier1][c])
+    } 
+    if (cartaCrupier2==10){
+        carta2CrupierImagen.setAttribute("src","baraja-img/" + baraja[cartaCrupier2][w])
+    } else {
         carta2CrupierImagen.setAttribute("src","baraja-img/" + baraja[cartaCrupier2][d])
     }
+
 
     menuInicial.style.display = "none";
     menuJugar.style.display = " inline-flex" 
@@ -187,7 +202,7 @@ function apostar() {
 function pedirCarta() {
     cartaNueva = generarNumeroCarta()
 
-     comprobarAs(carta1,carta2,cartaNueva)
+    comprobarAs(carta1,carta2,cartaNueva)
 
     console.log(cartaNueva)
     tuMano = tuMano + cartaNueva
